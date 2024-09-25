32d1216e04560c71a3abee42e26200dfd5479042
umar aurangzeb <umar12598@gmail.com>
2024-09-26 00:05:42 +0500

    home added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import Signup from './pages/Signup'
+import Home from './components/Home'
 export default function App() {
   return (
     <div>
+      <Home />
       {/* <SignIn /> */}
-      <Signup />
+      {/* <Signup /> */}
     </div>
   )
 }
